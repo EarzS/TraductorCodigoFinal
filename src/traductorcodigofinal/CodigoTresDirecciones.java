5f5acf405ddfe05e852ac339cc4eaf8699955acd
Hector <hectorlarez1@gmail.com>
2017-03-11 11:05:47 -0430

    Incorporacion de clase traductora y codigo de tres direcciones.

---
+++ src/traductorcodigofinal/CodigoTresDirecciones.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package traductorcodigofinal;
+
+/**
+ * Representacion del codigo de tres direcciones, el cual posee una etiqueta
+ * identificadora, y tres direcciones.
+ * 
+ * El codigo de tres direcciones viene de la forma: 
+ * instruccion direccion1, direccion2, direccion3
+ * 
+ * Es importante señalar la importancia del orden en el que se agregue las direcciones.
+ * 
+ * @author Hector
+ */
+public class CodigoTresDirecciones {
+    
+    /**
+     * Instruccion de codigo intermedio.
+     */
+    private String instruccion;
+
+    /**
+     * Primer direccion.
+     */
+    private String direccion1;
+
+    /**
+     * Segunda direccion.
+     */
+    private String direccion2;
+
+    /**
+     * Tercera direccion.
+     */
+    private String direccion3;
+
+    /**
+     * Constructor default.
+     */
+    public CodigoTresDirecciones(){
+    	this.instruccion = "CARGAR_VALOR";
+    	this.direccion1  = "15";
+    	this.direccion2	 = "null";
+    	this.direccion3  = "10000";
+    }
+
+    /**
+     * Constructor personalizado, donde validara el lexico y la sintaxis de la
+     * cadena, la semantica sera verificada desde el traductor.
+     */
+    public CodigoTresDirecciones(String cadena){
+    	// Validar la expresion regular.
+    	
+    }
+}
---
+++ src/traductorcodigofinal/Traductor.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package traductorcodigofinal;
+
+/**
+ * Modulo dedicado a traducir las cadenas de texto ingresadas por el lector
+ * de archivos.
+ * @author Hector
+ */
+public class Traductor {
+
+    /**
+     * Lee la cadena y compara el primer token con algun registro existente
+     * en la base de datos de comandos.
+     */
+    public static String traductor(String cadena){
+    	/* Todo - Validar la expresion regular de la cadena si es 
+    	   codigo de tres direcciones. */
+
+		// Obtencion de los tokens de la cadena
+    	String[] tokens = cadena.split("\\s");
+
+    	// Leer el primer token
+    	String primerToken = tokens[0];
+		/* Acceso la base de datos de traducciones
+			y solicito que me retorne la cadena a construir */
+		String cadenaTraduccion = traducciones.get(primerToken);
+		// En caso de no existir el token indico un error de sintaxis
+		if(cadenaTraduccion == null){
+			System.out.println("[Traductor.traductor()] Error semantico: " + primerToken + 
+			" no es una instruccion de codigo intermedio.");
+
+			return null;
+		}
+
+    	// Leer los demas tokens
+    		// Verifico en funcion del primer token temporal el comportamiento de
+    	 	// las variables
+    		// En funcion a su comportamiento, construyo la sentencia
+
+    	// Retorno la cadena final
+    }
+}
